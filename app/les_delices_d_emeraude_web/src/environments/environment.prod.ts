b7a24a4a929dfc3a16fe39421ee2d01005c7691d
Frederic Donfack <fredericdonfack13@gmail.com>
2026-05-26 17:36:07 -0400

    feat(environments): ajouter les fichiers de configuration pour les environnements de production et de développement

---
+++ app/les_delices_d_emeraude_web/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://kuvlgwbcawigjvjufswi.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dmxnd2JjYXdpZ2p2anVmc3dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMTEyNDQsImV4cCI6MjA5NDg4NzI0NH0.DQuuTiDDzW3BsnLAfCIJeWf9tSgl2-tBLQSS9hvVdGY'
+};
