375a159d12a437c7385a6453458c77b073428e6c
Frederic Donfack <fredericdonfack13@gmail.com>
2026-05-26 17:37:35 -0400

    feat(auth): ajouter le service d'authentification avec gestion de session et token feat(interceptors): ajouter un intercepteur pour gérer l'authentification des requêtes feat(routes): définir les routes de l'application avec des chemins commentés pour le développement

--- app/les_delices_d_emeraude_web/src/app/app.routes.ts
+++ app/les_delices_d_emeraude_web/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HomePageComponent },
+  // { path: 'products', component: ProductsPageComponent },
+  // { path: 'products/:slug', component: ProductDetailPageComponent },
+  // { path: 'login', component: LoginPageComponent },
+  // { path: 'register', component: RegisterPageComponent },
+  // {
+  //   path: 'dashboard',
+  //   canActivate: [AuthGuard],
+  //   component: DashboardLayoutComponent,
+  //   children: [
+  //     { path: 'orders', component: OrdersListPageComponent },
+  //     { path: 'orders/:id', component: OrderDetailPageComponent },
+  //     { path: 'profile', component: ProfilePageComponent },
+  //   ]
+  // },
+  // {
+  //   path: 'admin',
+  //   canActivate: [AdminGuard],
+  //   component: AdminLayoutComponent,
+  //   children: [ /* Sprint 3 */ ]
+  // },
+  { path: '**', redirectTo: '' }
+];
