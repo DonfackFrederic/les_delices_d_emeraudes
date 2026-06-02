8c95aa293e2cb7ced7825d328d089515dd2fed65
Frederic Donfack <fredericdonfack13@gmail.com>
2026-06-02 13:52:14 -0400

    feat(interceptors): réactiver l'intercepteur d'authentification avec gestion du token feat(homepage): améliorer la mise en page des sections de la page d'accueil

--- app/les_delices_d_emeraude_web/src/app/core/interceptors/auth-interceptor.ts
+++ app/les_delices_d_emeraude_web/src/app/core/interceptors/auth-interceptor.ts
@@ -4,16 +4,16 @@ import { inject } from '@angular/core';
 import { from, switchMap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  // const auth : Auth = inject(Auth);
-  // return from(auth.getToken()).pipe(
-  //     switchMap(token => {
-  //       if (token) {
-  //         req = req.clone({
-  //           setHeaders: { Authorization: `Bearer ${token}` }
-  //         });
-  //       }
-  //       return next(req);
-  //     })
-  //   );
+  const auth : Auth = inject(Auth);
+  return from(auth.getToken()).pipe(
+      switchMap(token => {
+        if (token) {
+          req = req.clone({
+            setHeaders: { Authorization: `Bearer ${token}` }
+          });
+        }
+        return next(req);
+      })
+    );
   return next(req);
 };
