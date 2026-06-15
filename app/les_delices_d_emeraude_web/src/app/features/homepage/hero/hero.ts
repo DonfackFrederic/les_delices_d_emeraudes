a946676bdb3600316f2128e30ae049f044e1d1c3
Frederic Donfack <fredericdonfack13@gmail.com>
2026-06-15 13:32:35 -0400

    fix(header): update navigation links to use routerLink and enhance header functionality

--- app/les_delices_d_emeraude_web/src/app/features/homepage/hero/hero.ts
+++ app/les_delices_d_emeraude_web/src/app/features/homepage/hero/hero.ts
@@ -1,9 +1,10 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
